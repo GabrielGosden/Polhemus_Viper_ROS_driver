a85ae1b81a4e1aa992a7009c684a8aca17ef9ec5
Gabriel <gbg_97@hotmail.com>
2020-11-13 16:09:11 +0100

    Implementation of safety features

--- src/path_generation.cpp
+++ src/path_generation.cpp
@@ -19,9 +19,15 @@
 
 /* Global variables*/
 double x,y,z,az,el,ro;
-int sample_number, sample_time, dist;
-double temp;
+int sample_number, sample_time, dist,state = 1, init_run = 0;
 char input_string[20];
+
+// Test variables. Delete later
+double temp;
+
+
+ros::Publisher ur_script_pub;
+
 
 // Callback function for kalman_filter_pose_ori_sub
 void kalman_filter_pose_ori_callback(const viper_tf2_broadcaster::viper_msg_pose_ori  &msg)
@@ -50,8 +56,6 @@ void viper_broadcaster_dist_callback(const viper_tf2_broadcaster::viper_msg_dist
 }
 
 
-double vel;
-
 int main(int argc, char **argv)
 {
 	// Create ROS node
@@ -65,16 +69,12 @@ int main(int argc, char **argv)
 	// Get distortion level from the viper
 	ros::Subscriber viper_broadcaster_dist_sub = n.subscribe("viper_broadcaster_dist",10,viper_broadcaster_dist_callback);
 	// Publisher for movel to UR
-	ros::Publisher ur_script_pub = n.advertise<std_msgs::String>("ur_hardware_interface/script_command",10);
-	
+	//ros::Publisher ur_script_pub = n.advertise<std_msgs::String>("ur_hardware_interface/script_command",10);
 	
 	// Get ROS loop_rate
 	n.getParam("/path_sample_time", sample_time);
 	ros::Rate loop_rate(sample_time);
 
-	//ROS_INFO("Please ensure Viper is configured and coordinates are correct. \n");
-	//ROS_INFO("Please place sensor in start position to ensure safe start. \n");
-	//ROS_INFO("When ready write 'run' to start robot.");
 
     while (ros::ok())
   {
@@ -82,26 +82,87 @@ int main(int argc, char **argv)
 	std_msgs::String msg;
 	std::stringstream ss;
 
+
 	// Fill string with position data and apply rotation matrix (Rot(x,pi)*Rot(z,pi/2))
 	//ss << "servoj(get_inverse_kin(p[" << y << "," << x << "," << z << "," << el << "," << ro << "," << az <<"]), t=0.08, lookahead_time=0.001, gain=100)";
-	ss << "servoj(get_inverse_kin(p[" << y << "," << x << "," << z << "," << el << "," << ro << "," << az <<"]), t=0.5, lookahead_time=0.001, gain=100)";
+	//ss << "servoj(get_inverse_kin(p[" << y << "," << x << "," << z << "," << el << "," << ro << "," << az <<"]), t=0.5, lookahead_time=0.001, gain=100)";
 	
 	// Put generated string into message
-	msg.data = ss.str();
-
+	//msg.data = ss.str();
+	//ss << "movej([-1.571,-1.571,1.571,-0.576,1.571,0],a=0.1,v=0.1)";
+	//msg.data = ss.str();
+	//ur_script_pub.publish(msg);
 	
-	// Safety for testing
-	ROS_INFO("x = %f, y = %f, z = %f, az = %f, el = %f, ro = %f",y,x,z,az,el,ro);
-
-
-	// If statements for limits.
-	if ( y > -0.5 && y < 0.6){ 		// Robot frame x lim
-		if( x > 0 && x < 0.9){ 		// Robot frame y lim
+	switch (state){
+		//Move robot to safe starting position
+		case 1:
+			ur_script_pub = n.advertise<std_msgs::String>("ur_hardware_interface/script_command",10);
+			ros::Duration(0.5).sleep();
+			ss << "movej([-1.571,-1.571,1.571,-0.576,1.571,0],a=0.1,v=0.1)";
+			msg.data = ss.str();
 			ur_script_pub.publish(msg);
+			ROS_INFO("Sending robot to starting position, please wait.");
+			state=2;
+
+		// Confirm by user that robot is at safe starting position.
+		case 2:
+			ROS_INFO("Confirm robot is at starting position by pressing 'Enter'.");
+			fgets(input_string,100,stdin);
+			state=3;
+		break;
+		
+		// Move sensor to safe starting position.
+		case 3: 
+			ROS_INFO("Please move Viper sensor to the safe starting position and orientation");
+			if(x > 0.5 && x < 0.55){
+				if(y > -0.40 && y < 0.40){
+					if(z > -0.40 && z < 0.40){
+							ROS_INFO("Sensor is in safe starting position.");
+							state = 4;
+					}else{
+						ROS_INFO("Sensor Z limit.");
+					}
+				}else{
+					ROS_INFO("Sensor X limit.");
+				}
+			}else{
+				ROS_INFO("Sensor Y limit.");
+			}
+		break;
+		// Confirm by user to start robot movement.
+		case 4:
+			ROS_INFO("To start robot movement press 'Enter'");
+			fgets(input_string,100,stdin);
+			state = 5;
+		break;
+
+		// Running the robot.
+		case 5:
+		if (init_run = 0){
+			ur_script_pub = n.advertise<std_msgs::String>("ur_hardware_interface/script_command",10);
+			ros::Duration(0.5).sleep();
+			init_run = 1;
 		}
-	}else{	
-		ROS_INFO("Position NOT sent, out of bounds!");
-		//asd
+		
+		if(x > 0 && x < 1.00){
+			if(y > -0.40 && y < 0.40){
+				if(z > -0.40 && z < 0.40){
+						ROS_INFO("x = %f, y = %f, z = %f, az = %f, el = %f, ro = %f",y,x,z,az,el,ro);
+						ss << "servoj(get_inverse_kin(p[" << y << "," << x << "," << z << "," << el << "," << ro << "," << az <<"]), t=0.08, lookahead_time=0.001, gain=100)";
+						msg.data = ss.str();
+						ur_script_pub.publish(msg);
+						//ROS_INFO("Within limits, sending command");
+				}else{
+					ROS_WARN("Robot stopped: Z limit.");
+				}
+			}else{
+				ROS_WARN("Robot stopped: X limit.");
+			}
+		}else{
+			ROS_WARN("Robot stopped: Y limit.");
+		}
+		break;
+
 	}
 	
 	ros::spinOnce();
--- src/path_generation_functions.h
+++
@@ -1,3 +0,0 @@
-
-double calc_velocity(double x, double y, double z,int sample_number);
-char getch();
